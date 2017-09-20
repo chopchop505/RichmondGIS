93b3b6f55c65ef2ce5ffd0d37474d5012401b60b
Joe Chop <joseph.chop@foreveroceans.com>
2017-09-19 20:49:48 -1000

    removed cats scaffolding

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { RoutingModule } from './routing.module';
 import { SharedModule } from './shared/shared.module';
-import { CatService } from './services/cat.service';
 import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
 import { AuthGuardLogin } from './services/auth-guard-login.service';
 import { AuthGuardAdmin } from './services/auth-guard-admin.service';
 import { AppComponent } from './app.component';
-import { CatsComponent } from './cats/cats.component';
 import { AboutComponent } from './about/about.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +18,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CatsComponent,
     AboutComponent,
     RegisterComponent,
     LoginComponent,
@@ -37,7 +34,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
     AuthService,
     AuthGuardLogin,
     AuthGuardAdmin,
-    CatService,
     UserService
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
